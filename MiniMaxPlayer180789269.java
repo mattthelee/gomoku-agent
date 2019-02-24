bd117416ae07e8248c55e19242ca30df9fab9f54
Matthew Lee <matt_lee92@hotmail.co.uk>
2019-02-24 14:38:11 +0000

    added saving of heuristci to bd and also starting move hardcoded

--- MiniMaxPlayer180789269.java
+++ MiniMaxPlayer180789269.java
@@ -22,11 +22,11 @@ import java.util.*;
     // Need To implement alpha-beta pruning - done
 
 class MiniMaxPlayer180789269 extends GomokuPlayer {
-    int[][] allRuns = new int[][]{{0, 32, 64, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {0, 1, 36, 65, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {0, 1, 2, 40, 66, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {0, 1, 2, 3, 44, 67, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {0, 1, 2, 3, 48, 80, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {1, 2, 3, 52, 81, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {2, 3, 56, 82, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {3, 60, 83, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {4, 32, 33, 68, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {4, 5, 36, 37, 64, 69, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {4, 5, 6, 40, 41, 65, 70, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {4, 5, 6, 7, 44, 45, 66, 71, 80, -1, -1, -1, -1, -1, -1, -1}, {4, 5, 6, 7, 48, 49, 67, 81, 84, -1, -1, -1, -1, -1, -1, -1}, {5, 6, 7, 52, 53, 82, 85, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {6, 7, 56, 57, 83, 86, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {7, 60, 61, 87, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {8, 32, 33, 34, 72, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {8, 9, 36, 37, 38, 68, 73, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {8, 9, 10, 40, 41, 42, 64, 69, 74, 80, -1, -1, -1, -1, -1, -1}, {8, 9, 10, 11, 44, 45, 46, 65, 70, 75, 81, 84, -1, -1, -1, -1}, {8, 9, 10, 11, 48, 49, 50, 66, 71, 82, 85, 88, -1, -1, -1, -1}, {9, 10, 11, 52, 53, 54, 67, 83, 86, 89, -1, -1, -1, -1, -1, -1}, {10, 11, 56, 57, 58, 87, 90, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {11, 60, 61, 62, 91, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {12, 32, 33, 34, 35, 76, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {12, 13, 36, 37, 38, 39, 72, 77, 80, -1, -1, -1, -1, -1, -1, -1}, {12, 13, 14, 40, 41, 42, 43, 68, 73, 78, 81, 84, -1, -1, -1, -1}, {12, 13, 14, 15, 44, 45, 46, 47, 64, 69, 74, 79, 82, 85, 88, -1}, {12, 13, 14, 15, 48, 49, 50, 51, 65, 70, 75, 83, 86, 89, 92, -1}, {13, 14, 15, 52, 53, 54, 55, 66, 71, 87, 90, 93, -1, -1, -1, -1}, {14, 15, 56, 57, 58, 59, 67, 91, 94, -1, -1, -1, -1, -1, -1, -1}, {15, 60, 61, 62, 63, 95, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {16, 32, 33, 34, 35, 80, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {16, 17, 36, 37, 38, 39, 76, 81, 84, -1, -1, -1, -1, -1, -1, -1}, {16, 17, 18, 40, 41, 42, 43, 72, 77, 82, 85, 88, -1, -1, -1, -1}, {16, 17, 18, 19, 44, 45, 46, 47, 68, 73, 78, 83, 86, 89, 92, -1}, {16, 17, 18, 19, 48, 49, 50, 51, 64, 69, 74, 79, 87, 90, 93, -1}, {17, 18, 19, 52, 53, 54, 55, 65, 70, 75, 91, 94, -1, -1, -1, -1}, {18, 19, 56, 57, 58, 59, 66, 71, 95, -1, -1, -1, -1, -1, -1, -1}, {19, 60, 61, 62, 63, 67, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {20, 33, 34, 35, 84, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {20, 21, 37, 38, 39, 85, 88, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {20, 21, 22, 41, 42, 43, 76, 86, 89, 92, -1, -1, -1, -1, -1, -1}, {20, 21, 22, 23, 45, 46, 47, 72, 77, 87, 90, 93, -1, -1, -1, -1}, {20, 21, 22, 23, 49, 50, 51, 68, 73, 78, 91, 94, -1, -1, -1, -1}, {21, 22, 23, 53, 54, 55, 69, 74, 79, 95, -1, -1, -1, -1, -1, -1}, {22, 23, 57, 58, 59, 70, 75, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {23, 61, 62, 63, 71, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {24, 34, 35, 88, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {24, 25, 38, 39, 89, 92, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {24, 25, 26, 42, 43, 90, 93, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {24, 25, 26, 27, 46, 47, 76, 91, 94, -1, -1, -1, -1, -1, -1, -1}, {24, 25, 26, 27, 50, 51, 72, 77, 95, -1, -1, -1, -1, -1, -1, -1}, {25, 26, 27, 54, 55, 73, 78, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {26, 27, 58, 59, 74, 79, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {27, 62, 63, 75, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {28, 35, 92, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {28, 29, 39, 93, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {28, 29, 30, 43, 94, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {28, 29, 30, 31, 47, 95, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {28, 29, 30, 31, 51, 76, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {29, 30, 31, 55, 77, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {30, 31, 59, 78, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, {31, 63, 79, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}};
     int maxBranching = 64;
-    int maxDepth = 3;
+    int maxDepth = 2;
     Color me;
     Color notMe;
+    HashMap<String,BoardAnalysis> analysedBoards = new HashMap<String,BoardAnalysis>();
 
 
     class BoardAnalysis {
@@ -38,14 +38,17 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
         float valueToWhite;
         int longestWhiteRun;
         int longestBlackRun;
+        String boardID;
 
-        BoardAnalysis (List<Move> legalMoves, Color winner, int[] whiteRuns, int[] blackRuns, int longestWhiteRun, int longestBlackRun){
+
+        BoardAnalysis (List<Move> legalMoves, Color winner, int[] whiteRuns, int[] blackRuns, int longestWhiteRun, int longestBlackRun, String boardID){
             this.whiteRuns = whiteRuns;
             this.blackRuns = blackRuns;
             this.legalMoves = legalMoves;
             this.winner = winner;
             this.longestWhiteRun = longestWhiteRun;
             this.longestBlackRun = longestBlackRun;
+            this.boardID = boardID;
         }
     }
 
@@ -61,20 +64,8 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
             bd.legalMoves = reorderMovesByHeuristic(board, this.me , bd.legalMoves);
             System.out.println("Number of Legal moves: " + bd.legalMoves.size());
 
-            if ( bd.legalMoves.size() ==62 && false){
-                Move testMove;
-                Color[][] cloneBoard2 = deepCloneBoard(board);
-                testMove = new Move(2,0);
-                cloneBoard2[testMove.row][testMove.col] = this.me;
-                float minVal = minABValue(cloneBoard2, me, Color.white, -2, 2, this.maxDepth);
-                System.out.println("***Best opposition move value " + minVal + " against: " + testMove.row + ":" + testMove.col);
-
-                Color[][] cloneBoard3 = deepCloneBoard(board);
-                testMove = new Move(0,1);
-                cloneBoard3[testMove.row][testMove.col] = this.me;
-                minVal = minABValue(cloneBoard3, me, Color.white, -2, 2, this.maxDepth);
-                System.out.println("***Best opposition move value " + minVal + " against: " + testMove.row + ":" + testMove.col);
-
+            if ( bd.legalMoves.size() ==64){
+                return new Move(4,4);
 
             }
 
@@ -99,16 +90,6 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
         }
     }
 
-    private Color[][] deepCloneBoard(Color[][] board){
-        Color[][] cloneBoard = new Color[8][8];
-        for (int col = 0; col < 8; col++ ) {
-            for (int row = 0; row < 8; row++) {
-                cloneBoard[row][col] = board[row][col];
-
-            }
-        }
-        return cloneBoard;
-    }
 
     private BoardAnalysis boardAnalyser(Color[][] board){
         // Checks for end of game and returns legalmoves if not ended
@@ -116,30 +97,50 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
         int[] blackRuns = new int[96];
         int longestWhiteRun = 0;
         int longestBlackRun = 0;
+        String boardID = "";
         List<Move> legalMoves = new ArrayList<>();
-        //System.out.println("*** DEBUG *** ");
-        longestWhiteRun = getMaxRunForBoard(board, Color.white);
-        if (longestWhiteRun >= 5) {
-            return new BoardAnalysis(legalMoves, Color.white, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun );
-        }
-        longestBlackRun = getMaxRunForBoard(board, Color.black);
-        if (longestBlackRun >= 5) {
-            return new BoardAnalysis(legalMoves, Color.black, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun );
-        }
+        BoardAnalysis bd;
+
         // Looping through each position is expensive so try to do only once
         for (int col = 0; col < 8; col++ ) {
             for (int row = 0; row < 8; row++) {
+                Color token = board[row][col];
                 // For each square
                 int squareID = row * 8 + col;
-                Color token = board[row][col];
                 if (token == null) {
                     // For every empty square
                     legalMoves.add(new Move(row, col));
+                    boardID = boardID + "0";
                     continue;
+                } else if (token == Color.white){
+                    boardID = boardID + "w";
+                } else {
+                    boardID = boardID + "b";
                 }
             }
         }
-        return new BoardAnalysis(legalMoves, null, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun );
+
+        if (this.analysedBoards.containsKey(boardID)){
+            //System.out.println("Loading: " + boardID);
+            return analysedBoards.get(boardID);
+        }
+
+        //System.out.println("*** DEBUG *** ");
+        longestWhiteRun = getMaxRunForBoard(board, Color.white);
+        if (longestWhiteRun >= 5) {
+            bd = new BoardAnalysis(legalMoves, Color.white, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun, boardID);
+            this.analysedBoards.put(boardID,bd);
+            return bd;
+        }
+        longestBlackRun = getMaxRunForBoard(board, Color.black);
+        if (longestBlackRun >= 5) {
+            bd = new BoardAnalysis(legalMoves, Color.black, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun, boardID);
+            this.analysedBoards.put(boardID,bd);
+            return bd;
+        }
+        bd = new BoardAnalysis(legalMoves, null, whiteRuns, blackRuns, longestWhiteRun, longestBlackRun, boardID);
+        this.analysedBoards.put(boardID,bd);
+        return bd;
     }
 
     private int getMaxRunForBoard(Color[][] board, Color player){
@@ -239,6 +240,7 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
         float value = (float) (whiteScore - blackScore) / (float) (whiteScore + blackScore);
         bd.valueToWhite = value;
         bd.valueToBlack = -value;
+        this.analysedBoards.put(bd.boardID,bd);
 
         if (player == Color.black){
             value = -value;
@@ -369,85 +371,14 @@ class MiniMaxPlayer180789269 extends GomokuPlayer {
         return value;
     }
 
-/*
+    private Color[][] deepCloneBoard(Color[][] board){
+        Color[][] cloneBoard = new Color[8][8];
+        for (int col = 0; col < 8; col++ ) {
+            for (int row = 0; row < 8; row++) {
+                cloneBoard[row][col] = board[row][col];
 
-    private int maxValue(Color[][] board, Color maxColor, Color minColor){
-
-        BoardAnalysis bd = boardAnalyser(board);
-        if (bd.winner != null) {
-            return -1; // stand in for infinity
-        }
-        List<Integer> values = new ArrayList<Integer>();;
-        for (Move legalMove : bd.legalMoves){
-            Color[][] cloneBoard = deepCloneBoard(board);
-            cloneBoard[legalMove.row][legalMove.col] = maxColor;
-            values.add(minValue(cloneBoard, maxColor, minColor));
-        }
-        return Collections.max(values);
-    }
-
-    private int minValue(Color[][] board, Color maxColor, Color minColor){
-
-        BoardAnalysis bd = boardAnalyser(board);
-        if (bd.winner != null) {
-            return 1; // stand in for infinity
-        }
-        List<Integer> values = new ArrayList<Integer>();;
-        for (Move legalMove : bd.legalMoves){
-            Color[][] cloneBoard = deepCloneBoard(board);
-            cloneBoard[legalMove.row][legalMove.col] = minColor;
-            values.add(maxValue(cloneBoard, maxColor, minColor));
-        }
-        return Collections.min(values);
-    }
-
-    private Move minimaxDecision(Color[][] board, Color me){
-        BoardAnalysis bd = boardAnalyser(board);
-        Move bestMove = bd.legalMoves.get(0);
-        int bestVal = -2;
-        Color minColor = (me == Color.white) ? Color.black : Color.white;
-
-        for (Move legalMove : bd.legalMoves){
-            Color[][] cloneBoard = deepCloneBoard(board);
-            cloneBoard[legalMove.row][legalMove.col] = me;
-            int minVal = minValue(board, me, minColor);
-            if ( minVal > bestVal){
-                bestVal = minVal;
-                bestMove = legalMove;
             }
         }
-        return  bestMove;
+        return cloneBoard;
     }
-
-    /*
-    private int[] minimax(int player, List<Move>  legalMoves, Color[][] board, Color me){
-        int winner;
-        int row = col = -1;
-
-
-    }
-
-/*
-    private int convertBoardToInt(Color[][] board){
-        int[][] newBoard;
-        for (int col = 0; col == 7; col++){
-            for (int row = 0; row == 7; row ++){
-                newBoard[col][row] = 0
-                Color var4 = this.turn == Color.white ? Color.black : Color.white;
-            }
-        }
-    }
-
-    public convolve(Color[][] filter, Color[][] board, Color me){
-        int filterWidth = filter.length;
-        int filterHeight = filter[0]length;
-
-        for (int col = 0; col == 7-filterWidth ; col ++ ){
-            for (int row = 0; row == 7; row ++){
-                // output[col][row] = my color if the convolution of the filter and the board is white, if black then is black else blank
-            }
-
-        }
-
-    } */
 }
